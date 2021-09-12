73b4c9449530b2bb1bb0b7eec0b1026c5896465b
Dmitry Kuksin <frantomas6677@gmail.com>
2021-09-12 19:47:02 +0300

    Изменен хост

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '127.0.0.1',
+                'host' => '46.175.146.87',
                 'port' => 6003,
                 'scheme' => 'http'
             ],
