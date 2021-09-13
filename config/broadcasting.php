8f69138224c7cee73862309d11d5560445ff8df7
Dmitry Kuksin <frantomas6677@gmail.com>
2021-09-13 23:09:49 +0300

    Включен ЭХО

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
                 'encrypted' => true,
-                'host' => '46.175.146.87',
+                'host' => '127.0.0.1',
                 'port' => 6003,
                 'scheme' => 'http',
             ],
