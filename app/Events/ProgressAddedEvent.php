bec7f27c150e9d718576ef0a90cb5baba5c11b33
Dmitry Kuksin <frantomas6677@gmail.com>
2021-09-16 18:10:52 +0300

    Включен ЭХО с настройками

--- app/Events/ProgressAddedEvent.php
+++ app/Events/ProgressAddedEvent.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ProgressAddedEvent implements ShouldBroadcastNow
+class ProgressAddedEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
