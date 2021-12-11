0a7eeecfee9c65ae27a381821d119bc7c9691119
kyksa byksa <kyksa@gmail.com>
2021-12-12 00:10:49 +0200

    Фикс джобса 1.4

--- app/Jobs/GroupJob.php
+++ app/Jobs/GroupJob.php
@@ -128,6 +128,8 @@ class GroupJob implements ShouldQueue
 
             $this->progress += $calculated_progress;
             $this->status = 'Получение постов';
+            echo $group->progress.PHP_EOL;
+            echo $this->progress.PHP_EOL;
             $group->progress = $this->progress;
             $group->status = $this->status;
             $group->save();
