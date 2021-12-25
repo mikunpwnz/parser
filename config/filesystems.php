3bc0e2c558ae275cbabe2d91dbcdc3f80686caa2
kyksa byksa <kyksa@gmail.com>
2021-12-25 14:23:23 +0200

    Изменен конфиг для сохранения файлов

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,6 +40,14 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0777
+                ],
+                'dir' => [
+                    'public' => 0777
+                ]
+            ],
         ],
 
         's3' => [
